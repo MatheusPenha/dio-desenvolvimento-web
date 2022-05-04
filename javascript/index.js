a55d5f4f7d3ea4df0051f46c088ce53a66491e3f
Matheus Penha <74919126+MatheusPenha@users.noreply.github.com>
2022-05-03 22:43:34 -0300

    Add files via upload

---
+++ javascript/index.js
@@ -0,0 +1,52 @@
+//(INTRODUÇÃO A ESSA BAGAÇA TODA)
+//var nome = "Matheus Penha ";
+//var idade = 21;
+//var idade2 = 10;
+//alert (nome + " tem " + idade + " anos "); aqui ele vai dizer seu nome e sobrenome e informar a idadde declarada neste alerta, não se esqueça de dar espaços entre as aspas se não o nome ficara colado nas aspas.
+// para fazer comentarios com javascript
+/*para fazer um comentario especifico com uma quantidade maior de linhas */
+
+//lembrar de sempre declarar variaveis (Var)
+//alert(idade + idade2 + " esse e o valor da idade de matheus daqui a dez anos "); //nessa variavel ele vai fazer o processo de soma das vaariaveis declaradas no caso idade 1 e 2, com isso ele irá informar em um alert a soma das idades.
+
+// Sei que não sou o unico no mundo que muitas vezes se acha inutil más confia em mim brother vai dar tudo certo não só eu mais você tambem vai se tornar um grande dev, estamos nessa caminhada para crescer e virar uma pessoa de incrivel nessa jornada.
+//console.log(nome);
+//console.log(idade + idade2)/*Aqui ele ira puxar o resultado das variaveis declaradas e irá mostrar no console do navegador, para visualizar clique com o botaão direito do ,mouse e vá na opção inspecionar e la encontrará vairas opções clique na penultima de baixo para cima "inspecionar." */
+
+//var frase = " Japão e o pior time do mundo ";
+//console.log(frase.replace(" Japão ", " Brasil ")); // a função replace ira substituir a varavel anterior declarada, motivo ? na real não sei talvez deva ser só coisa que quem inventou essa linguagem de modificar algo que esteja com preguiça de procurar e modificar. 😊
+//console.log(frase.toUppercase());irá colocar as frases em maiusculo o lower e o contrario rsrsrs. 
+/*
+var n1 = 12;
+var n2 = 55;
+console.log(n1 + n2);
+ */
+
+//(ARRAY)
+/*Caro imbecil retardado quando for declarar variavel coloca ela antes do sinal de "=", por que se colocar depois ele não vai validar nada, desde já grato. att matheus do passado*/
+//var lista = ["maça", "pera", "miojo", "carne", "vinho"];
+
+//lista.push("uva");// o push e para adicionar itens, o POP e para remover. Lembra de por esta merda dentro das aspas e tambem não esquece de fechar com ; //
+//lista.pop();
+//console.log(lista);//favor tambem não esquecer de que quando fechar tudo coloca o console.log no fim pq se por antes só vai até um ponto determinado do codigo.
+//var ortifruti = {nome: "maça", cor:"purple"};
+//console.log(ortifruti.nome, ortifruti.cor);//se deixar desse jeito fica sem cor e sem a lista
+//alert(ortifruti.cor); //aqui fica apenas o nome no alert 
+//console.log(ortifruti)// aparece lista e cor na tranquilidade 
+
+//(CONDICIONAIS)
+/*var idade = prompt("Qual a tua idade"); // aqui vai fazer uma pergunta se a resposta for maior que o valor declarado na variavel idade ele vai mostra o testo na tela, se for menor vai mostrar outra resposta.
+//var idade = 17; // aqui vai receber a idade
+if (idade >= 18){
+    alert("Olha só quem já é maior de idade já pode toma umas");
+}
+else {
+    alert("Caralho vc se fudeu ainda bem que foi vc e não eu, caralho vc se fudeu");
+}*/
+
+//(ESTRUTURA DE REPETIÇÃO)
+/*var count = 0;
+while(count <= 5){
+    console.log(count);
+    count = count +1 (OU SE PREFIRIR COUNT ++ TAMBEM VAI IGUAL, CONFIA ?)
+};*/ //estrutura de repetição com o comando while.
